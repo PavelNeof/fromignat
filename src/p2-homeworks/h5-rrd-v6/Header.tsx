7a72623d6e41921266632d01c6124829d39eb69e
PavelNeof <pavelneof@gmail.com>
2022-07-23 16:52:12 +0300

    HW4

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.header}>
 
+            <NavLink to={'/'} className={s.button5}>main</NavLink>
+
+            <NavLink to={'/pre-junior'} className={s.button5}>pre-junior</NavLink>
+
+            <NavLink to={'/junior'} className={s.button5}>junior</NavLink>
+
+            <NavLink to={'/junior-plus'} className={s.button5}>junior-plus</NavLink>
         </div>
     )
 }
